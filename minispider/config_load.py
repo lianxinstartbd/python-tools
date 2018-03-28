af4ec90e4c7660c0d2377ce14485c007d6c05f6c
lianxinstartbd <oopscooper@163.com>
2018-03-28 17:14:29 +0800

    Update config_load.py

--- minispider/config_load.py
+++ minispider/config_load.py
@@ -2,7 +2,7 @@
 #-*-coding: utf-8 -*-
 """
   @FileName : config_load.py
-  @Author : wangqun02(wangqun02@baidu.com)
+  @Author : lianxin
   @CreateTime : 2016-11-25 20:10
   @Last modified : 2016-12-03 12:10
   @Description : load config and urls
